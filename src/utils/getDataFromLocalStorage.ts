11f9d69326b226f52dcf9a2a8c81dd2256b0125a
Sergey <graygmai1@gmail.com>
2022-10-09 16:33:11 +0500

    fix(getUserTheme): fix LocalStorage methods

--- src/utils/getDataFromLocalStorage.ts
+++ src/utils/getDataFromLocalStorage.ts
@@ -1,17 +1,16 @@
 /**
- * Возвращает объект, если данные по ключу были в локал сторедж.
+ * Возвращает объект, если данные по ключу были в локал сторедж? то объект с данными.
  *
  * @param {string} LSKey нужный Ключ в локал сторедж
- * @return {any | undefined} , либо undefined, если такого ключа не существует, либо обект, который находился по данном ключу
+ * @return {any} , либо пустой обект, если такого ключа не существует, либо сущьность, которыая находилась по данном ключу
  */
-const getDataFromLocalStorage = (LSKey: string): any | undefined => {
-	try {
-		const objFromLS = localStorage.getItem(LSKey);
-
-		return objFromLS ? JSON.parse(objFromLS) : undefined;
-	} catch (error) {
-		return undefined;
-	}
+const getDataFromLocalStorage = (LSKey: string): any => {
+  const objFromLS = localStorage.getItem(LSKey);
+  try {
+    return JSON.parse(objFromLS ?? "{}");
+  } catch (e) {
+    return {};
+  }
 };
 
 export default getDataFromLocalStorage;
